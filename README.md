f936eef9e5a29617b5721ef9cb6d6717020ec2ab
Hiroshi SUGIMURA <hiroshi.sugimura@gmail.com>
2022-06-03 17:12:43 +0900

    0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,147 @@
-# usb-2jcie-bu.js
-This module provides OMRON USB device (2JCIE-BU).
+# Overview
+
+このモジュールは**OMRONのUSB環境センサ(2JCIE-BU)**をサポートします．
+非公式ですので、本モジュールに関してOMRON社様に問い合わせなどは行わないようにお願いします．
+
+This module provides **USB environment sensor (2JCIE-BU) producted by OMRON**.
+This module is informality.
+
+
+動作確認は 2JCIE-BU01 (https://www.fa.omron.co.jp/products/family/3724/lineup.html) で行いました．
+OMRON 公式ページより、USB ドライバーをダウンロードして PC にインストールしてください。
+
+
+NPMを見ると他にも開発されていますが、本モジュールの特徴は node-gyp に依存しないことです．
+Windows以外では利点にならないかもしれません．
+
+
+
+# Install
+
+
+次に下記コマンドでモジュールをインストールできます．
+
+You can install the module as following command.
+
+
+```bash
+npm i usb-2jcie-bu
+```
+
+
+# Demos
+
+## Controller demo
+
+
+Here is a demonstration script.
+
+```JavaScript:Demo
+'use strict';
+
+const omron = require('usb-2jcie-bu');
+const cron = require('node-cron');
+require('date-utils');
+
+
+omron.start(  (sensorData) => {
+	console.log( '----------------------------' );
+	let dt = new Date();
+	console.log( dt );
+	console.dir( sensorData );
+});
+
+
+// 2秒毎にチェック
+cron.schedule('*/2 * * * * *', () => {
+	omron.requestData();
+});
+```
+
+
+
+# Data stracture
+
+```JavaScript:stracture
+sensorData: {
+	'sequence_number': sequence_number,
+	'temperature': temperature,
+	'humidity': humidity,
+	'anbient_light': anbient_light,
+	'pressure': pressure,
+	'noise': noise,
+	'etvoc': etvoc,
+	'eco2': eco2,
+	'discomfort_index': discomfort_index,
+	'heat_stroke': heat_stroke
+}
+```
+
+
+# APIs
+
+## 初期化と実行
+
+- start(callback)
+受信したらcallback関数を呼び出します。
+callback関数は下記の形式です。
+
+```
+callback( sensorData )
+```
+
+
+## データのリクエスト
+
+- requestData()
+
+
+# 攻略情報
+
+定期的にデータを取得するにはcronモジュールを活用するとよいです。
+
+```
+const omron = require('usb-2jcie-bu');
+const cron = require('node-cron');
+
+// 2秒毎にチェック
+cron.schedule('*/2 * * * * *', () => {
+	omron.requestData();
+});
+```
+
+# meta data
+
+## Authors
+
+神奈川工科大学  創造工学部  ホームエレクトロニクス開発学科; Dept. of Home Electronics, Faculty of Creative Engineering, Kanagawa Institute of Technology
+
+杉村　博; SUGIMURA, Hiroshi
+
+## thanks
+
+Thanks to Github users!
+
+- 参考にしたソース、Reference
+https://github.com/futomi/html5-omron-2jcie-bu
+
+
+## License
+
+MIT License
+
+```
+-- License summary --
+o Commercial use
+o Modification
+o Distribution
+o Private use
+x Liability
+x Warranty
+```
+
+
+## Log
+
+- 0.0.2 ちょっとできたので公開
+- 0.0.1 開発開始
---
+++ index.js
@@ -0,0 +1,183 @@
+//////////////////////////////////////////////////////////////////////
+//	Copyright (C) Hiroshi SUGIMURA 2022.06.03
+//////////////////////////////////////////////////////////////////////
+'use strict';
+
+const { SerialPort } = require('serialport');
+const cron = require('node-cron');
+
+
+let omron = {
+	callback: null,
+	portConfig: {
+		path: 'COM3',
+		baudRate: 115200,
+		dataBits: 8,
+		stopBits: 1,
+		parity: 'none'
+	},
+	port: null,
+
+	//////////////////////////////////////////////////////////////////////
+	// リクエストデータ生成 (Uint8Array)
+	createRequestData: function () {
+		// Header
+		const header_view = new Uint8Array([0x52, 0x42]);
+		// Length (Payload ～ CRC-16)
+		const length_view = new Uint16Array([5]);
+		// Payload frame
+		const command_view = new Uint8Array([0x01]); // 0x01: Read, 0x02: Write
+		const address_view = new Uint16Array([0x5022]); // 0x5022: Latest data short
+		// CRC-16 (Header ～ Payload)
+		const crc = omron.calcCrc16([header_view, length_view, command_view, address_view]);
+		const crc_view = new Uint16Array([crc]);
+		// 各 Typed Array を結合して 1 つの Uint8 Typed Array にする
+		const req_data = omron.concatTypedArrays([header_view, length_view, command_view, address_view, crc_view]);
+
+		return req_data;
+	},
+
+	//////////////////////////////////////////////////////////////////////
+	// Typed Array オブジェクトのリストを 1 つの Uint8Array に連結
+	concatTypedArrays: function (typed_array_list) {
+		let byte_list = [];
+		for (let typed_array of typed_array_list) {
+			let uint8_view = new Uint8Array(typed_array.buffer, 0);
+			for (let byte of uint8_view) {
+				byte_list.push(byte);
+			}
+		}
+		return new Uint8Array(byte_list);
+	},
+
+	//////////////////////////////////////////////////////////////////////
+	// Typed Array のリストから CRC-16 を算出
+	calcCrc16: function(typed_array_list) {
+		let byte_list = omron.concatTypedArrays(typed_array_list);
+		let reg = 0xffff;
+		for (let i = 0; i < byte_list.length; i++) {
+			reg = reg ^ byte_list[i];
+			let bit_shift = 0;
+			while (true) {
+				let last_bit = reg & 1;
+				reg = reg >>> 1;
+				if (last_bit === 1) {
+					reg = reg ^ 0xA001;
+				}
+				bit_shift++;
+				if (bit_shift >= 8) {
+					break;
+				}
+			}
+		}
+		return reg;
+	},
+
+	//////////////////////////////////////////////////////////////////////
+	// レスポンスをパース
+	parseResponse: function( recvData ) {
+		if (recvData[0] !== 0x52 || recvData[1] !== 0x42) {
+			return;
+		}
+
+		// 本当は recvData.buffer で行けると思うけど、過去データが呼び出されるという謎のバグに悩まされ、変換を2回をする中でバイト数調整をして何とかした
+		let data_view = new DataView( Uint8Array.from(recvData).subarray(0,30).buffer );
+		let len = data_view.getUint16(2, true);
+		if (len !== recvData.byteLength - 4) {
+			console.log('レスポンスのバイト長異常を検知したため受信データを破棄しました: ' + len + ',' + recvData.byteLength);
+			return;
+		}
+
+		let command = data_view.getUint8(4);
+		let address = data_view.getUint16(5, true);
+		if (address !== 0x5022) {
+			console.log('レスポンスのアドレスが未知のため受信データを破棄しました: address=' + address);
+			return;
+		}
+
+		let sequence_number  = data_view.getUint8(7);
+		let temperature      = data_view.getInt16(8, true) / 100; // degC
+		let humidity         = data_view.getInt16(10, true) / 100; // %RH
+		let anbient_light    = data_view.getInt16(12, true); // lx
+		let pressure         = data_view.getInt32(14, true) / 1000; // hPa
+		let noise            = data_view.getInt16(18, true) / 100; // dB
+		let etvoc            = data_view.getInt16(20, true); // ppb
+		let eco2             = data_view.getInt16(22, true); // ppm
+		let discomfort_index = data_view.getInt16(24, true) / 100;
+		let heat_stroke      = data_view.getInt16(26, true) / 100; // degC
+
+
+		// 出力
+		omron.callback( { 'sequence_number': sequence_number,
+			'temperature': temperature, 'humidity': humidity, 'anbient_light': anbient_light, 'pressure': pressure, 'noise': noise,
+			'etvoc': etvoc, 'eco2': eco2, 'discomfort_index': discomfort_index, 'heat_stroke': heat_stroke});
+	},
+
+
+	//////////////////////////////////////////////////////////////////////
+	// シリアルポートのリスト取得
+	getPortList: async function () {
+		let portList = [];
+
+		await SerialPort.list()
+			.then( (ports) => {
+				portList = ports;
+			}) .catch( (err) => {
+				console.log(err, "e")
+			});
+
+		return portList;
+	},
+
+	requestData: function () {
+		omron.port.write( omron.createRequestData() );
+	},
+
+
+	//////////////////////////////////////////////////////////////////////
+	// entry point
+	start: async function ( callback ) {
+		omron.portConfig = {  // default config set
+			path: 'COM3',
+			baudRate: 115200,
+			dataBits: 8,
+			stopBits: 1,
+			parity: 'none'
+		};
+		omron.port = null;
+
+		if( callback ) {
+			omron.callback = callback;
+		}else{
+			console.error( 'usb-2jcie-bu.start(): callback is null' );
+			throw new Error('usb-2jcie-bu.start(): callback is null');
+		}
+
+		// 環境センサーに接続
+		// ユーザーにシリアルポート選択画面を表示して選択を待ち受ける
+		let portList = await omron.getPortList();
+		let com = await portList.filter( (p) => {
+			if( p.vendorId == '0590' && p.productId == '00D4') {
+				return p;
+			}
+		});
+
+		omron.portConfig.path = com[0].path;
+
+		omron.port = new SerialPort( omron.portConfig, function (err) {
+			if (err) {
+				return console.log('Error: ', err.message)
+			}
+		});
+
+		omron.port.on('data', function (recvData) {
+			omron.parseResponse( recvData );
+		});
+	}
+};
+
+
+module.exports = omron;
+//////////////////////////////////////////////////////////////////////
+// EOF
+//////////////////////////////////////////////////////////////////////
